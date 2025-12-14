9afb9e531f8526bb5ef8e42ff306de753e4a3d27
unknown <cdh2895924329@gmail.com>
2025-12-14 22:14:18 +0800

    修复报错

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    # 'django.contrib.staticfiles'
 
     # app应用
     'blog.apps.BlogConfig',
